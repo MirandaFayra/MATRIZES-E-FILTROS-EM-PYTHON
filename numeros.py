29657a4d87f49eff158f1ff7e37e6b18f09eb93a
Fayra Miranda <miranda.fayra@gmail.com>
2019-09-05 20:19:06 -0300

    Criar Pasta Exercicios

---
+++ numeros.py
@@ -0,0 +1,17 @@
+# APLICAÇÃO PARA IMPUTAR NÚMEROS
+
+n1 = input ('Digite um número: ')
+n2 = input ('Digite um número: ')
+
+n1 = int (n1)
+n2 = int (n2)
+
+if n1 > n2:
+    print (str(n1) +' é maior que ' + str(n2))
+    
+elif n1 < n2:
+    print (str(n1) +' é menor que ' + str(n2))
+    
+else:
+  print('os números são iguais')
+    
